669370b5531276f4d80d49fb0ba61217733f07c0
Anton Matchev <akm99@cornell.edu>
2023-10-26 02:46:28 -0400

    Update Cards Module - Found a good ASCII representation for cards - Created a deck as a card list - Created a function that lets you draw cards - Create a function that lets you print the entire deck - Create a function that lets you print a list of cards - Considering adding a separate deck module for all of these functions - Had trouble loading code in utop/running it

--- cards.ml
+++ cards.ml
@@ -1,43 +1,27 @@
-module Cards1 = struct
+module Cards = struct
   type suit = Hearts | Diamonds | Clubs | Spades
-  type rank = int
-  type card = { rank : rank; suit : suit }
+  type card = { rank : int; suit : suit }
 
   let ranks = [ 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12; 13; 14 ]
   let suits = [ Hearts; Diamonds; Clubs; Spades ]
 
-  let string_of_suit = function
-    | Hearts -> "♥"
-    | Diamonds -> "♦"
-    | Clubs -> "♣"
-    | Spades -> "♠"
+  let suit_of_string str =
+    match String.capitalize_ascii str with
+    | "Hearts" -> Some Hearts
+    | "Diamonds" -> Some Diamonds
+    | "Clubs" -> Some Clubs
+    | "Spades" -> Some Spades
+    | _ -> None
 
-  let string_of_rank = function
-    | 11 -> "J"
-    | 12 -> "Q"
-    | 13 -> "K"
-    | 14 -> "A"
-    | n -> string_of_int n
-
-  let create_deck () =
-    let product l1 l2 =
-      List.concat (List.map (fun e1 -> List.map (fun e2 -> (e1, e2)) l2) l1)
-    in
-    List.map (fun (r, s) -> { rank = r; suit = s }) (product ranks suits)
-
-  let print_card card =
-    Printf.printf "%s%s " (string_of_rank card.rank) (string_of_suit card.suit)
-
-  let print_deck deck = List.iter print_card deck
-
-  let () =
-    let deck = create_deck () in
-    print_deck deck
-end
-
-module Cards2 = struct
-  type suit = Hearts | Diamonds | Clubs | Spades
-  type card = { rank : int; suit : suit }
+  let rank_to_string r =
+    if r < 2 || r > 14 then None
+    else
+      match r with
+      | 11 -> Some "J"
+      | 12 -> Some "Q"
+      | 13 -> Some "K"
+      | 14 -> Some "A"
+      | n -> Some (string_of_int n)
 
   let suit_to_string = function
     | Hearts -> "♥"
@@ -64,7 +48,43 @@ module Cards2 = struct
       \  +---------+" (rank_to_string card.rank) (suit_to_string card.suit)
       (rank_to_string card.rank)
 
-  let () =
-    let example_card = { rank = 10; suit = Diamonds } in
-    print_endline (card_to_ascii example_card)
+  let create_deck () =
+    let product l1 l2 =
+      List.concat
+        (List.map
+           (fun e1 -> List.map (fun e2 -> { rank = e1; suit = e2 }) l2)
+           l1)
+    in
+    product ranks suits
+
+  let print_card r s =
+    let is_valid_rank r = r >= 2 && r <= 14 in
+    let is_valid_suit s =
+      match s with Hearts | Diamonds | Clubs | Spades -> true
+    in
+
+    if is_valid_rank r && is_valid_suit s then
+      let card = { rank = r; suit = s } in
+      print_endline (card_to_ascii card)
+    else print_endline "Invalid card"
+
+  let empty_deck = []
+  let full_deck = create_deck ()
+
+  let rec print_deck = function
+    | [] -> print_endline ""
+    | { rank; suit } :: t ->
+        print_card rank suit;
+        print_deck t
+
+  let print_full_deck = print_deck full_deck
+
+  let draw_card (deck : card list) : card * card list =
+    match deck with
+    | [] -> failwith "Empty deck, cannot draw a card"
+    | _ ->
+        let drawn_index = Random.int (List.length deck) in
+        let drawn_card = List.nth deck drawn_index in
+        let rest = List.filter (fun c -> c <> drawn_card) deck in
+        (drawn_card, rest)
 end
