592e67a6027bde54bb8e22e3d308ebaf04caa2dc
Anton Matchev <akm99@cornell.edu>
2023-11-27 15:00:31 -0500

    Implement hand_category and Comparison of hands - Wrote a function that gets all possible hand combinations for a given board and player hand - Wrote the  hand_category function that assigns a hand to a set of five cards - Wrote the compare_hands function that compares different hands of poker accordingly - Wrote possible hands that gets the list of all possible hand categories a player can achieve with their two cards and the board - Currently in progress of writing "best_hand"

--- lib/hands.ml
+++ lib/hands.ml
@@ -48,13 +48,20 @@ let sort_by_rank (hand : card list) = List.rev (List.sort compare_by_rank hand)
 let cards_to_ranks (hand : card list) = List.map (fun card -> card.rank) hand
 
 (* Function that gets all the possible hands on the board a player could have *)
-(* let possible_hands (board : card list) (player_hand : card list) = let rec
-   card_combos lst = let rec card_combos_aux acc len lst = if len = 0 then [ []
-   ] else match lst with | [] -> acc | h :: t -> let rec combo_map acc f =
-   function | [] -> acc | h :: t -> combo_map (f h :: acc) f t in let gen =
-   combo_map acc (fun z -> h :: z) (card_combos_aux [] (len - 1) t) in
-   card_combos_aux gen len t in card_combos_aux [] 5 lst in card_combos [ board
-   @ player_hand ] *)
+let card_combinations (board : card list) (player_hand : card list) =
+  let rec combinations k lst =
+    if k = 0 then [ [] ]
+    else
+      match lst with
+      | [] -> []
+      | hd :: tl ->
+          let with_hd =
+            List.map (fun rest -> hd :: rest) (combinations (k - 1) tl)
+          in
+          let without_hd = combinations k tl in
+          with_hd @ without_hd
+  in
+  combinations 5 (board @ player_hand)
 
 let flush_checker (hand : card list) =
   let rec flush_helper = function
@@ -218,6 +225,130 @@ let high_card (hand : card list) : category option =
              hcard5 = e.rank;
            })
   | _ -> None
+
+let hand_category (hand : card list) : category =
+  match is_royal_flush hand with
+  | Some rf -> rf
+  | None -> (
+      match is_straight_flush hand with
+      | Some sf -> sf
+      | None -> (
+          match is_four_of_kind hand with
+          | Some fkind -> fkind
+          | None -> (
+              match is_full_house hand with
+              | Some fh -> fh
+              | None -> (
+                  match is_flush hand with
+                  | Some fl -> fl
+                  | None -> (
+                      match is_straight hand with
+                      | Some s -> s
+                      | None -> (
+                          match is_three_of_kind hand with
+                          | Some tkind -> tkind
+                          | None -> (
+                              match is_two_pair hand with
+                              | Some tpair -> tpair
+                              | None -> (
+                                  match is_pair hand with
+                                  | Some p -> p
+                                  | None -> (
+                                      match high_card hand with
+                                      | Some high -> high
+                                      | None -> failwith "Imposible")))))))))
+
+let compare_hands (h1 : category) (h2 : category) : int =
+  match (h1, h2) with
+  | Royal_Flush, Royal_Flush -> 0
+  | Royal_Flush, _ -> 1
+  | _, Royal_Flush -> -1
+  | Straight_Flush s1, Straight_Flush s2 -> compare s1.hcard s2.hcard
+  | Straight_Flush _, _ -> 1
+  | _, Straight_Flush _ -> -1
+  | Four_Of_A_Kind f1, Four_Of_A_Kind f2 ->
+      if compare f1.four_kind f2.four_kind = 0 then compare f1.hcard f2.hcard
+      else compare f1.four_kind f2.four_kind
+  | Four_Of_A_Kind _, _ -> 1
+  | _, Four_Of_A_Kind _ -> -1
+  | Full_House fh1, Full_House fh2 ->
+      let threes = compare fh1.three_kind fh2.three_kind in
+      if threes = 0 then compare fh1.pair fh2.pair else threes
+  | Full_House _, _ -> 1
+  | _, Full_House _ -> -1
+  | Flush f1, Flush f2 ->
+      let c1 = compare f1.hcard1 f2.hcard1 in
+      if c1 = 0 then
+        let c2 = compare f1.hcard2 f2.hcard2 in
+        if c2 = 0 then
+          let c3 = compare f1.hcard3 f2.hcard3 in
+          if c3 = 0 then
+            let c4 = compare f1.hcard4 f2.hcard4 in
+            if c4 = 0 then compare f1.hcard5 f2.hcard5 else c4
+          else c3
+        else c2
+      else c1
+  | Flush _, _ -> 1
+  | _, Flush _ -> -1
+  | Straight s1, Straight s2 -> compare s1.hcard s2.hcard
+  | Straight _, _ -> 1
+  | _, Straight _ -> -1
+  | Three_Of_A_Kind t1, Three_Of_A_Kind t2 ->
+      let threes = compare t1.three_kind t2.three_kind in
+      if threes = 0 then
+        let hc = compare t1.hcard1 t2.hcard1 in
+        if hc = 0 then compare t1.hcard2 t2.hcard2 else hc
+      else threes
+  | Three_Of_A_Kind _, _ -> 1
+  | _, Three_Of_A_Kind _ -> -1
+  | Two_Pair tp1, Two_Pair tp2 ->
+      let p1 = compare tp1.pair1 tp2.pair1 in
+      if p1 = 0 then
+        let p2 = compare tp1.pair2 tp2.pair2 in
+        if p2 = 0 then compare tp1.hcard tp2.hcard else p2
+      else p1
+  | Two_Pair _, _ -> 1
+  | _, Two_Pair _ -> -1
+  | One_Pair p1, One_Pair p2 ->
+      let pair = compare p1.pair p2.pair in
+      if pair = 0 then
+        let hc1 = compare p1.hcard1 p2.hcard1 in
+        if hc1 = 0 then
+          let hc2 = compare p1.hcard2 p2.hcard2 in
+          if hc2 = 0 then compare p1.hcard3 p2.hcard3 else hc2
+        else hc1
+      else pair
+  | One_Pair _, _ -> 1
+  | _, One_Pair _ -> -1
+  | High h1, High h2 ->
+      let hc1 = compare h1.hcard1 h2.hcard1 in
+      if hc1 = 0 then
+        let hc2 = compare h1.hcard2 h2.hcard2 in
+        if hc2 = 0 then
+          let hc3 = compare h1.hcard3 h2.hcard3 in
+          if hc3 = 0 then
+            let hc4 = compare h1.hcard4 h2.hcard4 in
+            if hc4 = 0 then compare h1.hcard5 h2.hcard5 else hc4
+          else hc3
+        else hc2
+      else hc1
+
+let possible_hands (board : card list) (player_hand : card list) : category list
+    =
+  List.map hand_category (card_combinations board player_hand)
+
+let best_player_hand (board : card list) (player_hand : card list) : category =
+  let pos_hands = possible_hands board player_hand in
+  match pos_hands with
+  | [] -> failwith "Not possible"
+  | c1 :: rest ->
+      List.fold_left
+        (fun hand1 hand2 ->
+          if compare_hands hand1 hand2 < 0 then hand2 else hand1)
+        c1 rest
+
+let best_hand (board : card list) (player1 : 'a) (player2 : 'a) =
+  failwith "Unimplemented"
 
 (* (**[one_pair_draft] returns rank of a pair in list of cards if pair exists,
    otherwise returns none. Returns: option of rank of pair if pair exists
